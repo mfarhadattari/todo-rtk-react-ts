574397375d62f5ad21720349ab4033e22c16d802
Mohammad Farhad <mfarhad.attari@gmail.com>
2024-01-12 17:19:50 +0600

    remove todo functonality implemented

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,8 +1,10 @@
-import { ITodo } from "@/redux/features/todoSlice";
+import { ITodo, removeTodo } from "@/redux/features/todoSlice";
+import { useAppDispatch } from "@/redux/hooks";
 import { Pencil2Icon, TrashIcon } from "@radix-ui/react-icons";
 import { Button } from "../ui/button";
 
 const TodoCard = ({ todo }: { todo: ITodo }) => {
+  const dispatch = useAppDispatch();
   return (
     <div className="flex justify-between p-3 bg-slate-900 text-white rounded-md">
       <input type="checkbox" name="check" id="check" />
@@ -10,7 +12,7 @@ const TodoCard = ({ todo }: { todo: ITodo }) => {
       <p>{new Date(todo.dateTime).toLocaleString()}</p>
       <p>{todo.description}</p>
       <div className="flex justify-center items-center">
-        <Button>
+        <Button onClick={() => dispatch(removeTodo(todo._id))}>
           <TrashIcon className="size-6 text-red-600" />
         </Button>
         <Button>
--- src/redux/features/todoSlice.ts
+++ src/redux/features/todoSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export interface ITodo {
   _id: string;
@@ -21,12 +21,15 @@ const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
-    addTodo: (state, action: { type: string; payload: ITodo }) => {
+    addTodo: (state, action: PayloadAction<ITodo>) => {
       state.todos.push({ ...action.payload, isCompleted: false });
+    },
+    removeTodo: (state, action: PayloadAction<string>) => {
+      state.todos = state.todos.filter((todo) => todo._id !== action.payload);
     },
   },
 });
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, removeTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
